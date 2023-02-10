37e405b6da533bb6a0978bf66607a968030b676b
benitamarya <benitamarya@gmail.com>
2023-02-10 11:15:30 -0500

    initial commit

---
+++ fullStack/project_1/Angular/routing-demo/src/app/course/course.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { courseService } from 'src/services/couseservice';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+  courses: any;
+  title: any;
+  author: any;
+  public static courseid: any;
+  public static moduleid: any;
+
+
+  selectLesson(lesson: any){
+    this.selectedLesson = lesson;
+    console.log("Lesson Selected")
+  }
+  selectedLesson = {
+    lesson:[] as any[],
+    id: undefined
+  }
+
+  selectedModule ={
+    lesson:[] as any[],
+    id: undefined
+  }
+
+selectModule(module: any) {
+this.selectedModule = module;
+  CourseComponent.moduleid = module.id;
+  console.log("Selecetd Module : ",CourseComponent.moduleid)
+}
+
+selectCourse(course: any) {
+  this.selectedCourse = course;
+
+  CourseComponent.courseid = course.id;
+  console.log("SELECTED COURSE : ",CourseComponent.courseid);
+}
+selectedCourse = {
+  modules:[] as any[],
+  id: undefined
+};
+
+  constructor(private courseService: courseService, private router: Router) { }
+
+  ngOnInit(): void {
+    
+    this.courseService.findallCourse().then(courses  => this.courses = courses)
+    console.log("I am in course component checking courses value : ",this.courses);
+
+  }
+
+
+
+}
---
+++ fullStack/project_1/Angular/routing-demo/src/services/couseservice.ts
@@ -0,0 +1,91 @@
+//import { HttpClient } from "@angular/common/http";
+import { Injectable, Input } from "@angular/core";
+
+@Injectable()
+export class courseService {
+  
+    findallCourse=()=>
+    fetch('http://localhost:8080/api/v1/courses').then(Response =>Response.json())
+    //fetch('http://courseservice-env.eba-eikexim3.us-east-1.elasticbeanstalk.com/api/v1/courses').then(Response =>Response.json());
+
+    
+    async addcourse(course:any ) {
+      let response = await fetch('http://localhost:8080/api/v1/courses/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify(course)
+        });
+        
+        alert("Course Added!");
+              
+  }
+    /*async addcourse(course:any ) {
+      await fetch('http://courseservice-env.eba-eikexim3.us-east-1.elasticbeanstalk.com/api/v1/courses/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify(course)
+        });
+        
+        alert("Course Added!");
+              
+  }
+  async addmodule(title: any, id: any){
+    console.log("Add Module with course id : ",id," and Title : ",title)
+    
+    await fetch(`http://courseservice-env.eba-eikexim3.us-east-1.elasticbeanstalk.com/api/v1/courses/${id}/module/`,{
+      method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify({"title":title})
+    })
+    alert("Module Added!");
+  }*/
+
+      async addmodule(title: any, id: any) {
+      console.log("Add Module with Course id = ",id," and Title : ",title);
+      let response = await fetch('http://localhost:8080/api/v1/courses/${id}/module/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify({"title":title})
+        });
+        
+        alert("Module Added!");
+              
+  }
+
+  /*async addlesson(title: any, id: any){
+    console.log("Add Lesson with Module id : ",id," and Title : ",title)
+    let response=await fetch(`http://courseservice-env.eba-eikexim3.us-east-1.elasticbeanstalk.com/api/v1/${id}/lesson`,{
+      method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+          body: JSON.stringify({"title":title})
+    })
+    alert("Lesson Added!");
+  }
+  */
+
+  async addlesson(title: any, id: any ) {
+    let response = await fetch('http://localhost:8080/api/v1/${id}/lesson', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({"title":title})
+      });
+      
+      alert("Lesson Added!");
+            
+}
+
+ 
+
+}
